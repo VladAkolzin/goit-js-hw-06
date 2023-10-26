6aa0a8440f9dcf7e50c653efe0338497fb3bb27a
VladAkolzin <akolzin95@gmail.com>
2023-10-26 17:33:28 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+input.addEventListener("input", (event) => {
+  const value = event.currentTarget.value;
+  if (value === "") {
+    output.textContent = "Anonymous";
+  } else {
+    output.textContent = event.currentTarget.value;
+  }
+});
