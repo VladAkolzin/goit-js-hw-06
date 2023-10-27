d6ba93df1e5f0bc5882d999944620462d92626be
VladAkolzin <akolzin95@gmail.com>
2023-10-27 14:49:59 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorBtn = document.querySelector(".change-color");
+const colorOutput = document.querySelector(".color");
+colorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  colorOutput.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+});
