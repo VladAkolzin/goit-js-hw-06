b5437569919c00ca9fa4d0c10bfddb23e91741cf
VladAkolzin <akolzin95@gmail.com>
2023-10-27 12:15:54 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
-
+const Logform = document.querySelector(".login-form");
+Logform.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const login = form.elements.email.value;
+  const pass = form.elements.password.value;
+  if (login === "" || pass === "") {
+    alert("Заповніть усі поля для введення даних");
+  } else {
+    const user = {
+      email: login,
+      password: pass,
+    };
+    form.reset();
+  }
+}
